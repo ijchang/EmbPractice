7ad42fa74d14f0ba44a320f7e394f448fe614a97
IJ Chnag <ij.chang@microchip.com>
2020-10-20 09:14:27 +0800

    add uint8_t to int8_t transformation

--- main.c
+++ main.c
@@ -1,2 +1,21 @@
+#include "stdint.h" 
+#include "stdio.h"
 /*add main.c*/
+
+int main(void)
+{
+    uint8_t i,k;
+    int8_t j;
+    
+
+    //j = -30;
+    //i = (uint8_t)j;
+    i = 129;
+    j = -i;
+    k = j;
+    printf("uint8_t i = %hhx\n", i);
+    printf("int8_t j = %hhx\n", j);
+    printf("uint8_t k = %hhx\n", k);
+    return 1;
+}
 
